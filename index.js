b9d86b2937fd83151aa4c008aae5c88cd8e000ba
Sean Clifford <sclifford6262@gmail.com>
2018-05-08 23:54:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 
-let neighborhoodId = 0
+let neighborhoodIdCounter = 0
 
 class Neighborhood {
 
   constructor(name){
     this.name = name
-    this.id = ++neighborhoodId
+    this.id = ++neighborhoodIdCounter
     store.neighborhoods.push(this)
   }
 
