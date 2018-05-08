51dde87b690ebf0e71e02f283edf21d5e8e45fdb
Sean Clifford <sclifford6262@gmail.com>
2018-05-08 23:48:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
 let neighborhoodId = 0
 
 class Neighborhood {
+
   constructor(name){
     this.name = name
     this.id = ++neighborhoodId
   }
+  
 }
